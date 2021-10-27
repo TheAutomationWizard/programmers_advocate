65d5f355fd9f4f260bcefb47412088e75f80ae8d
Aditya kumar <aditya.kumar@newfold.com>
2021-10-27 10:50:37 +0530

    comments

--- programmersadvocate/settings.py
+++ programmersadvocate/settings.py
@@ -144,6 +144,7 @@ MEDIA_ROOT = BASE_DIR /'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# ToDO : On Production - apply below changes...
 if os.getcwd() == '/app':
     # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # SECURE_SSL_REDIRECT = True
